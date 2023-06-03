eaad419b9916f990a31f16ab2158415c926603e1
Roberto Bellido <a25664@svalero.com>
2023-06-03 11:58:25 +0200

    Added main part of home

---
+++ src/components/main/Main.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="relative">
+      <div class="h-screen bg-cover bg-center bg-backg-home">
+        <div class="flex flex-col items-center justify-center h-screen">
+          <h1 class="text-4xl text-white font-bold">BarVision</h1>
+          <p class="text-2xl text-white mt-4">Cheers to Sports Lovers, Cheers to BarVision!</p>
+          <router-link to="/search"
+            class="bg-white text-black px-8 py-2 mt-8 transition-colors duration-500 hover:bg-gray-800 hover:text-white">Search
+            Now</router-link>
+        </div>
+      </div>
+  
+      <div class="bg-gray-900 py-8">
+        <div class="container mx-auto">
+          <h2 class="text-3xl font-semibold text-center mb-10 text-white mt-8">Suggested Matches</h2>
+          <div class="grid grid-cols-3 gap-8">
+            <div class="flex flex-col items-center">
+              <img class="w-full max-w-custom h-72 object-cover mb-6" src="../../assets/partidofutbol.jpg"
+                alt="Partido de Fútbol">
+              <p class="text-lg font-semibold text-white mt-2">Nombre del Partido de Fútbol</p>
+            </div>
+            <div class="flex flex-col items-center">
+              <img class="w-full max-w-custom h-72 object-cover mb-6" src="../../assets/partidotenis.jpg"
+                alt="Partido de Tenis">
+              <p class="text-lg font-semibold text-white mt-2">Nombre del Partido de Tenis</p>
+            </div>
+            <div class="flex flex-col items-center">
+              <img class="w-full max-w-custom h-72 object-cover mb-6" src="../../assets/partidobaloncesto.jpg"
+                alt="Partido de Baloncesto">
+              <p class="text-lg font-semibold text-white mt-2">Nombre del Partido de Baloncesto</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+  .max-w-custom {
+    max-width: 85%;
+  }
+  </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Main />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/main/Main.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Main
+  }
+}
+</script>
