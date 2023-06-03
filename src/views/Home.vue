838e960afcf46835020c4824ee43d910f7492dc8
Roberto Bellido <a25664@svalero.com>
2023-06-03 13:55:33 +0200

    Fixed home

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="back">
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+</template>
+  
+<script>
+// @ is an alias to /src
+import { Header } from "@/components/header/header.vue";
+import Main from "@/components/main/Main.vue";
+import Footer from "@/components/footer/Footer.vue";
+export default {
+    name: "Home",
+    components: { Header, Main, Footer },
+};
+</script>
+<style>
+.back {
+    overflow-y: auto;
+    height: 100vh;
+}
+</style>
+  
