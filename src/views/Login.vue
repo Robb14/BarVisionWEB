1c7fb8e2270e512b3df998cd82c23ee39cd01803
Roberto Bellido <a25664@svalero.com>
2023-06-03 14:02:16 +0200

    Fixed login

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="back-img-login relative justify-center flex items-center">
+        <Login class="absolute" />
+    </div>
+</template>
+  
+<script>
+import Login from "@/components/login/Login.vue";
+export default {
+    name: "Login",
+    components: { Login },
+    methods: {},
+};
+</script>
+<style scoped>
+.back-img-login {
+    background-image: url(../assets/fondologin.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+}
+</style>
