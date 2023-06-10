0806a758ddcc00ddaead9ce9a5bd6552a320e068
Roberto Bellido <a25664@svalero.com>
2023-06-10 19:41:32 +0200

    fixes

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -2,8 +2,8 @@
     <footer class="bg-white text-black py-14">
         <div class="container mx-auto py-6 px-4">
             <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                <div class="flex flex-col items-center sm:items-start">
-                    <h4 class="text-lg font-semibold">About us</h4>
+                <div class="flex flex-col items-center sm:items-stretch">
+                    <h4 class="text-lg font-semibold text-center">About us</h4>
                     <ul class="mt-4 space-y-2">
                         <li><a href="#">Terms & Conditions</a></li>
                         <li><a href="#">Contact</a></li>
@@ -11,7 +11,7 @@
                     </ul>
                 </div>
                 <div class="flex flex-col items-center justify-center mt-4 sm:mt-0">
-                    <h4 class="text-lg font-semibold">BarVision</h4>
+                    <h4 class="text-lg font-semibold text-center">BarVision</h4>
                     <ul class="mt-4 space-y-2">
                         <li>Tel 123-456-7890</li>
                         <li>Email info@barvision.com</li>
@@ -29,8 +29,8 @@
                     </div>
                 </div>
                 <div class="flex flex-col items-center sm:items-end mt-4 sm:mt-0">
-                    <h4 class="text-lg font-semibold">Subscribe to our Newsletter</h4>
-                    <div class="mt-4 flex">
+                    <h4 class="text-lg font-semibold text-center">Subscribe to our Newsletter</h4>
+                    <div class="mt-4 flex justify-center sm:justify-end">
                         <input type="email" placeholder="Enter your email"
                             class="border border-black px-4 py-2 rounded-l-md focus:outline-none">
                         <button
