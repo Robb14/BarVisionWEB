26861ae8b45cb473ec35a42e303d48ffbbfd7e6f
Roberto Bellido <a25664@svalero.com>
2023-06-11 16:21:22 +0200

    store done

--- src/store/index.js
+++ src/store/index.js
@@ -1,64 +1,40 @@
-// import axios from "axios";
-// import { createStore } from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
-// const store = createStore({
-//   state: {
-//     isLoggedIn: false,
-//     user: null,
-//     locations: [],
-//     token: null,
-//   },
-//   mutations: {
-//     setLoggedIn(state, payload) {
-//       state.isLoggedIn = payload;
-//     },
-//     setToken(state, payload) {
-//       state.token = payload;
-//     },
-//     setLocations(state, payload) {
-//       state.locations = payload;
-//     },
-//   },
-//   actions: {
-//     locations() {},
-//     login(context, credentials) {
-//       return new Promise((resolve, reject) => {
-//         axios
-//           .post(
-//             `https://barvisionapi-test.azurewebsites.net/Users/auth/login?name=${credentials.Username}&password=${credentials.Password}`
-//           )
-//           .then((response) => {
-//             console.log(response.data);
-//             const token = response.data;
-//             console.log(token);
-//             localStorage.setItem("username", credentials.Username);
-//             sessionStorage.setItem("token", JSON.stringify(token));
-//             context.commit("setLoggedIn", true);
-//             context.commit("setToken", token);
-//             resolve(response);
-//           })
-//           .catch((error) => {
-//             reject(error);
-//           });
-//       });
-//     },
-//   },
-//   getters: {
-//     isLoggedIn: (state) => {
-//       console.log(state.isLoggedIn);
-//       return state.isLoggedIn;
-//     },
-//     // currentUser: (state) => {
-//     //   console.log(state.user);
-//     //   return state.user;
-//     // },
-//     getToken(state) {
-//       return state.token;
-//     },
-//     getLocations(state) {
-//       return state.locations;
-//     },
-//   },
-// });
+Vue.use(Vuex);
 
-// export default store;
+const store = new Vuex.Store({
+    state: {
+        users: [],
+        loggedInUser: null,
+    },
+    mutations: {
+        SET_USERS(state, users) {
+            state.users = users;
+        },
+        SET_LOGGED_IN_USER(state, user) {
+            state.loggedInUser = user;
+        },
+    },
+    actions: {
+        async fetchUsers({ commit }) {
+            try {
+                const response = await axios.get(`${API_BASE_URL}/api/user`);
+                commit('SET_USERS', response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async login({ commit }, credentials) {
+            try {
+                const response = await axios.post(`${API_BASE_URL}/api/user/login`, credentials);
+                commit('SET_LOGGED_IN_USER', response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+});
+
+export default store;
